2e961a9dfa40609a5bd7371a0fc0641094762865
sebcuellar6 <seabasscuellarh@gmail.com>
2024-05-10 11:35:21 -0500

    first event listener

---
+++ script.js
@@ -0,0 +1,10 @@
+const rollButton = document.querySelector("#rollButton")
+const dice1 = document.querySelector("#dice1")
+const dice2 = document.querySelector("#dice2")
+const outComeDisplay = document.querySelector("#outcomeDisplay")
+
+
+
+rollButton.addEventListener('click', () => {
+    console.log("working")
+})
