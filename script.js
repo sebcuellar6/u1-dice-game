9d9238991a17ba1e20e91e42bb128041dbfd27a4
sebcuellar6 <seabasscuellarh@gmail.com>
2024-05-10 11:44:38 -0500

    numbers in dice

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const dice1 = document.querySelector("#dice1")
 const dice2 = document.querySelector("#dice2")
 const outComeDisplay = document.querySelector("#outcomeDisplay")
 
-
+function rollDice() {
+    return Math.floor(Math.random() * 6) +1
+}
 
 rollButton.addEventListener('click', () => {
-    console.log("working")
+   let newNum = rollDice()
+   dice1.innerText = `${rollDice()}`
+   dice2.innerText = `${rollDice()}`
+
+    
 })
